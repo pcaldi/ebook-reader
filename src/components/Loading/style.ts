fd63ae7bbb936bcd6c1f5925c382e4bd4026c6f9
pcaldi <pcaldi@gmail.com>
2023-12-05 18:05:43 -0300

    component loading

--- src/components/Loading/style.ts
+++ src/components/Loading/style.ts
@@ -6,6 +6,6 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: THEME.COLORS.GREY_800
+    backgroundColor: THEME.COLORS.GREY_300
   }
 });
