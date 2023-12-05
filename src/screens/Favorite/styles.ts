61cdacf8f17b6d08daa615d6f20c75e9d3055a62
pcaldi <pcaldi@gmail.com>
2023-12-05 18:06:57 -0300

    change bg

--- src/screens/Favorite/styles.ts
+++ src/screens/Favorite/styles.ts
@@ -1,9 +1,10 @@
 import { StyleSheet } from "react-native";
+import { THEME } from "../../styles/theme";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: THEME.COLORS.WHITE,
     alignItems: 'center',
     justifyContent: 'center',
   },
