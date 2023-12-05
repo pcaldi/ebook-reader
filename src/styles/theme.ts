793bee1fe72abe9d65fc64d83c9c53b5d6972cd1
pcaldi <pcaldi@gmail.com>
2023-12-05 18:05:16 -0300

    theme app

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,14 +3,11 @@ export const THEME = {
     WHITE: "#FFFFFF",
 
     GREY_100: "#E1E1E6",
-    GREY_300: "#8D8D99",
-    GREY_400: "#444",
-    GREY_500: "#505059",
-    GREY_600: "#323238",
-    GREY_700: "#29292E",
-    GREY_800: "#202024",
-    GREY_900: "#9999",
+    GREY_200: "#8D8D99",
+    GREY_300: "#444",
+    GREY_400: "#9999",
 
+    RED: '#E83F5B',
 
   },
   FONTS: {
