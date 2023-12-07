5bd93a513dac8724f24e8ffd1610a4d2e6fd6b21
pcaldi <pcaldi@gmail.com>
2023-12-06 21:38:42 -0300

    add header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,15 @@
 import { Books } from '@components/Books';
+import { Header } from '@components/Header';
+
+import { THEME } from '@styles/theme';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export function Home() {
   return (
-
-    <Books />
+    <SafeAreaView style={{ flex: 1, backgroundColor: THEME.COLORS.BLUE, padding: 24 }}>
+      <Header />
+      <Books />
+    </SafeAreaView>
 
   )
 };
